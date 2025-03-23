beff1d5a829dcf5bacace4a4cbfaab216c2ed093
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-23 21:51:31 +0000

    Created necessary migration tables

---
+++ backAura/database/migrations/2025_03_23_205354_create_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('senderEmail');
+            $table->text('messageText');
+            $table->dateTime('dateSent');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
---
+++ backAura/database/migrations/2025_03_23_205355_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->string('category');
+            $table->date('date');
+            $table->string('sourceCodeLink')->nullable();
+            $table->string('liveSiteLink')->nullable();
+            $table->foreignId('portfolio_id')->constrained('portfolios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
