20a4df4fb913853a70cd85c6264715b451701e6b
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-27 12:25:58 +0000

    Organized Routes for better appearance

--- backAura/routes/api.php
+++ backAura/routes/api.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\SkillController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\PortfolioController;
 
-
+// A U T H
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
+// P O R T F O L I O
 Route::apiResource('portfolios', PortfolioController::class);
 Route::get('portfolios/{portfolio}/owner', [PortfolioController::class, 'owner']);
 Route::get('portfolios/{portfolio}/projects', [PortfolioController::class, 'projects']);
@@ -18,12 +18,11 @@ Route::get('portfolios/{portfolio}/skills', [PortfolioController::class, 'skills
 Route::get('portfolios/{portfolio}/achievements', [PortfolioController::class, 'achievements']);
 Route::get('portfolios/{portfolio}/statistics', [PortfolioController::class, 'statistics']);
 Route::get('portfolios/{portfolio}/apis', [PortfolioController::class, 'apis']);
-
+// P R O J E C T S
 Route::apiResource('projects', ProjectController::class);
 Route::get('projects/category/{category}', [ProjectController::class, 'filterByCategory']);
 Route::get('projects/technology/{skill}', [ProjectController::class, 'filterByTechnology']);
-
-
+// S K I L L S
 Route::apiResource('skills', SkillController::class);
 Route::post('skills/{skill}/attach', [SkillController::class, 'attachToProject']);
 Route::post('skills/{skill}/detach', [SkillController::class, 'detachFromProject']);
