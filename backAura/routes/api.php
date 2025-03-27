0747cd15295af0bf408121dc484647dfe3caa1c0
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-27 12:01:07 +0000

    Added Projects Routes

--- backAura/routes/api.php
+++ backAura/routes/api.php
@@ -18,3 +18,7 @@ Route::get('portfolios/{portfolio}/skills', [PortfolioController::class, 'skills
 Route::get('portfolios/{portfolio}/achievements', [PortfolioController::class, 'achievements']);
 Route::get('portfolios/{portfolio}/statistics', [PortfolioController::class, 'statistics']);
 Route::get('portfolios/{portfolio}/apis', [PortfolioController::class, 'apis']);
+
+Route::apiResource('projects', ProjectController::class);
+Route::get('projects/category/{category}', [ProjectController::class, 'filterByCategory']);
+Route::get('projects/technology/{skill}', [ProjectController::class, 'filterByTechnology']);
