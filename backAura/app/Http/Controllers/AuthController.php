69a8d43bc2060d930975325be8d5e32bb07d2636
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-04 21:47:36 +0000

    Added register method to the AuthController

--- backAura/app/Http/Controllers/AuthController.php
+++ backAura/app/Http/Controllers/AuthController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
+    /**
+     * Register User & Create his Portfolio
+     */
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'job' => 'required|string|max:255',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            
+            $user = User::create($validated);
+            $user->portfolio()->create([
+                'title' => "{$user->name}'s Portfolio",
+                'owner_id' => $user->id
+            ]);
+            
+            DB::commit();
+            
+            return response()->json([
+                'user' => $user->only('id', 'name', 'email', 'job'),
+            ], 201);
+            
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['message' => 'Registration failed'], 500);
+        }
+    }
+
+
     /**
      * Login user and create token
      */
@@ -17,20 +51,19 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
-
         ]);
-
+    
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'message' => 'Invalid login credentials'
             ], 401);
         }
-
+    
         $user = $request->user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
+        $token = $user->createToken('aura_token')->plainTextToken;
+    
         return response()->json([
-            'user' => $user,
+            'user' => $user->only('id', 'name', 'email', 'job', 'photo'),
             'token' => $token,
         ]);
     }
@@ -53,9 +86,16 @@ class AuthController extends Controller
     public function user(Request $request)
     {
         return response()->json([
-            'user' => $request->user()->load(['portfolio' => function($query) {
-                $query->withCount(['projects', 'skills', 'achievements']);
-            }])
+            'user' => $request->user()->load([
+                'portfolio' => function($query) {
+                    $query->withCount(['projects', 'achievements', 'visitors', 'messages'])
+                          ->with([
+                              'statistics',
+                              'projects.technologies',
+                              'skills'
+                          ]);
+                }
+            ])
         ]);
     }
 }
