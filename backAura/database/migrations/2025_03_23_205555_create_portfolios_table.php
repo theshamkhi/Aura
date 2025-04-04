94cad142701acea2c50146c0f3a60e9ddbd56614
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-04 21:39:40 +0000

    Updated Database

--- backAura/database/migrations/2025_03_23_205555_create_portfolios_table.php
+++ backAura/database/migrations/2025_03_23_205555_create_portfolios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
