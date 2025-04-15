c43cea50da7ddb6b00265a15806ef24e1a6afeb5
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-15 16:30:02 +0100

    Added necessary routes with Auth protection and theme provider

--- frontAura/src/App.jsx
+++ frontAura/src/App.jsx
@@ -1,30 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { PrivateRoute } from './components/PrivateRoute';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
-import { Dashboard } from './pages/Dashboard';
-import { Navigate } from 'react-router-dom';
+import { DashboardLayout } from './layouts/DashboardLayout';
+import { Overview } from './pages/Overview';
+import { Projects } from './pages/Projects';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/dashboard"
-            element={
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            
+            <Route path="/dashboard" element={
               <PrivateRoute>
-                <Dashboard />
+                <DashboardLayout />
               </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
+            }>
+              <Route index element={<Overview />} />
+              <Route path="projects" element={<Projects />} />
+              {/* <Route path="skills" element={<Skills />} />
+              <Route path="achievements" element={<Achievements />} />
+              <Route path="messages" element={<Messages />} />
+              <Route path="settings" element={<Settings />} /> */}
+            </Route>
+
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
