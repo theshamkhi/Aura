250a4cc4d83ff5993893bf0530ba9f1478dc9bd6
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-15 16:20:06 +0100

    Enhanced Loading styling with a circular progress from MUI

--- frontAura/src/components/PrivateRoute.jsx
+++ frontAura/src/components/PrivateRoute.jsx
@@ -1,10 +1,20 @@
+import { CircularProgress, Box } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
-
-  if (loading) return <div>Loading...</div>;
+  
+  if (loading) return (
+    <Box sx={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh' 
+    }}>
+      <CircularProgress size={60} />
+    </Box>
+  );
   
   return user ? children : <Navigate to="/login" replace />;
 };
