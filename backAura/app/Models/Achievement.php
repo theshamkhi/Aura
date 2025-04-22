2a1a46e3e3f066b2a79f3674074f9f5185ed027d
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-22 16:34:16 +0100

    Fixed NULL date issue by moving date attribue from casts to fillable

--- backAura/app/Models/Achievement.php
+++ backAura/app/Models/Achievement.php
@@ -6,14 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Achievement extends Model
 {
-    protected $fillable = ['title', 'description', 'image_url', 'portfolio_id'];
-
-    protected function casts(): array
-    {
-        return [
-            'date' => 'date',
-        ];
-    }
+    protected $fillable = ['title', 'description', 'image_url', 'date', 'portfolio_id'];
 
     public function portfolio() {
         return $this->belongsTo(Portfolio::class);
